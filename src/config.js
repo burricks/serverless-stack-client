2cf82fb9b12e853ab94a7b7935f10eff2992726b
Lisa Dina <lisa.dina@spglobal.com>
2020-06-28 19:54:07 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "tutorial-scratchnotetakingapp"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://3dmdelykz1.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_KSULUkbft",
+      APP_CLIENT_ID: "tu6f7mt20n5orrbiur1ojq25h",
+      IDENTITY_POOL_ID: "us-east-1:b53231c9-3902-41b3-a65e-7ba4302daa37"
+    }
+  };
